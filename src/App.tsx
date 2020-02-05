6ef3a1539418972f9d0c520bea307478e8d459d9
Maulik Khandwala <maulikk2000@gmail.com>
2020-02-06 02:03:08 +1100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Router, Route } from "react-router";
+import HomePage from "./HomePage";
+import Navbar from "react-bootstrap/Navbar";
+import { createBrowserHistory as createHistory } from "history";
+import Nav from "react-bootstrap/Nav";
 
-const App = () => {
+const history = createHistory();
+const App = (contatcsStore: any) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}></Router>
     </div>
   );
-}
+};
 
 export default App;
